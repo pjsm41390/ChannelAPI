93c54a02ffb66235129a79c266a84538a517d1f6
PJ Singh <82594863+pjsm41390@users.noreply.github.com>
2021-10-21 13:51:16 -0400

    Add project files.

---
+++ ChannelAPI/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using Openfin.Desktop;
+using OpenfinDesktop;
+namespace ChannelAPI
+{
+    class Program
+    {
+        public static Runtime _runtime;
+
+        static void Main(string[] args)
+        {
+            MakeProvider();
+            Console.ReadLine();
+        }
+
+        public static void MakeProvider()
+        {
+            Action connected = new Action(ConnectChannel);
+
+            _runtime = Runtime.GetRuntimeInstance(new RuntimeOptions
+            {
+                Version = "stable"
+            });
+            _runtime.Connect(connected);
+        }
+
+        public async static void ConnectChannel()
+        {
+            var providerBus = _runtime.InterApplicationBus.Channel.CreateProvider("channelName");
+            providerBus.Opened += (identity, payload) =>
+            {
+                Console.WriteLine("Client connection request identity: ", identity?.ToString());
+                Console.WriteLine("Client connection request payload: ", payload?.ToString());
+            };
+
+            providerBus.RegisterTopic<Object>("topic", (payload, client) =>
+            {
+                Console.WriteLine("Action dispatched by client: ", client.RemoteEndpoint);
+                Console.WriteLine("Payload sent in dispatch: ", payload?.ToString());
+            });
+            await providerBus.OpenAsync();
+            var client = _runtime.InterApplicationBus.Channel.CreateClient("channelName");
+            await client.ConnectAsync();
+            await client.DispatchAsync("topic");
+        }
+    }
+}
